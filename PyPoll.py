b62042578cb0872fe912334e51c7eea8ab99775b
joZecodes <95896417+joZecodes@users.noreply.github.com>
2022-01-10 21:13:46 -0700

    Getting started
    Added pseudocode

---
+++ PyPoll.py
@@ -0,0 +1,7 @@
+# Data goals needed for project
+# Total number of votes cast
+# A complete list of candidates who received votes
+# Total number of votes each candidate received
+# Percentage of votes each candidate won
+# The winner of the election based on popular vote
+
