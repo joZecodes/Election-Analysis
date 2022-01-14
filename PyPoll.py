dca32e7665b32322e6191095913247cb88cafa49
joZecodes <95896417+joZecodes@users.noreply.github.com>
2022-01-14 14:09:00 -0700

    Add files via upload
    Get total votes

--- PyPoll.py
+++ PyPoll.py
@@ -9,9 +9,13 @@
 import csv
 import os
 # Assign a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+#file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = "election_results.csv"
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a total vote counter.
+total_votes = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -19,4 +23,12 @@ with open(file_to_load) as election_data:
 
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+    
+    
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+# 3. Print the total votes.
+print(total_votes) 
