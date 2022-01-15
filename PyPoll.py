7d0e1e9aaef97b0c60168dae826f72c8f3e6d90d
joZecodes <zachary.r.dom@gmail.com>
2022-01-15 11:30:24 -0700

    determine the winning candidate by the number and percentage of votes

--- PyPoll.py
+++ PyPoll.py
@@ -25,6 +25,10 @@ candidate_votes = {'Charles Casper Stockham': total_votes,
                     'Diana DeGette': total_votes,
                     'Raymon Anthony Doane': total_votes}
 
+#Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -62,8 +66,29 @@ for candidate_name in candidate_votes:
     #  Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
     #  Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
-            
+    # print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
+
+         # To do: print out each candidate's name, vote count, and percentage of
+        # votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)           
 
 # # . Print the candidate list
-# print(candidate_votes) 
+# print(candidate_votes)
