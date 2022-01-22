a88820e7c947940f5f996db74a52c70a3b9f7f57
joZecodes <zachary.r.dom@gmail.com>
2022-01-22 08:59:23 -0700

    get county count and calculate percentage

--- PyPoll_Challenge_starter_code.py
+++ PyPoll_Challenge_starter_code.py
@@ -94,14 +94,17 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
-
+    for county in county_votes:
         # 6b: Retrieve the county vote count.
-
+        county_vote = county_votes[county]
         # 6c: Calculate the percentage of votes for the county.
-
+        county_percentage = float(county_vote) / float(total_votes) * 100
 
          # 6d: Print the county results to the terminal.
-
+        county_results = (
+            f"{county}: {county_percentage}% ({county_vote:,})\n"
+        )
+        print(county_results, end="")
          # 6e: Save the county votes to a text file.
 
          # 6f: Write an if statement to determine the winning county and get its vote count.
